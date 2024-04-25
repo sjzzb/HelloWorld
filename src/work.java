c6b00ea656b0fd4dac3d80b9e7823b18b38849fb
bing <sjzzbforever@qq.com>
2024-04-25 14:45:43 +0800

    第一次提交

---
+++ src/work.java
@@ -0,0 +1,7 @@
+public class work {
+    public static void main(String[] args) {
+        System.out.println("周冰");
+        System.out.println("22物联网01");
+        System.out.println("2022133276");
+    }
+}
